5cd46cc5c8416d5bc157e78c8c820b65a7f5aaf0
Said Gaforov <sgaforov@gmail.com>
2023-01-11 20:32:10 -0700

    Class16 - refactor 04_DropdownTest2 Class

--- src/Class16_DevelopFunctions/_04_DropdownTest2.java
+++ src/Class16_DevelopFunctions/_04_DropdownTest2.java
@@ -2,6 +2,7 @@ package Class16_DevelopFunctions;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
 
@@ -15,13 +16,30 @@ public class _04_DropdownTest2  {
 
         // Method 1 ==> without Select class  (we need to locate ALL children within the dropdown == List<WebElement>)
         List<WebElement> listOfMonths = driver.findElements(By.cssSelector("select[name='Month'] option"));
-        selectDdValue(listOfMonths, "March");
+        for (WebElement month : listOfMonths) {
+            String actualText = month.getText(); // or .getValue();
+            if (actualText.equals(expectedText)) {
+                month.click();
+                break;
+            }
+        }
 
         // Method 2 ==> use Select Class (if DD is built with Select class and has options, use this approach).
         // We only need to locate parent of dropdown (not Options) == single WebElement
         WebElement monthsDD = driver.findElement(By.cssSelector("select[name='Month']"));
-        selectDdValue(monthsDD, expectedText);   // Selecting by visible text
+        Select select = new Select(monthsDD);
+        select.selectByIndex(4);
+        List<WebElement> options = select.getOptions();
+        for (WebElement option : options) {
+            if (option.getText().equals(expectedText)) {
+                select.selectByVisibleText(expectedText);
+                break;
+            }
+        }
 
+        // Calling both Method 1 & Method 2 from the CommonMethods Class.
+        selectDdValue(monthsDD, expectedText);   // Selecting by visible text
+        selectDdValue(listOfMonths, "March");
         selectDdValue(monthsDD, 55);                   // Selecting by index
         selectDdValue(driver.findElement(By.cssSelector("select[name='country']")), 400);
 
