f353ea2b1cf9c4e103bbdde6db4be38a523dadaa
Said Gaforov <sgaforov@gmail.com>
2023-01-30 20:15:40 -0700

    Class19 - infinite scroll

--- src/Class19_Screenshots_Actions_JS/_12_InfiniteScroll_Refactor.java
+++ src/Class19_Screenshots_Actions_JS/_12_InfiniteScroll_Refactor.java
@@ -7,7 +7,7 @@ import utils.BaseClass;
 
 import java.util.List;
 
-public class _11_InfiniteScroll_Refactor extends BaseClass {
+public class _12_InfiniteScroll_Refactor extends BaseClass {
     public static void main(String[] args) {
         setUp("https://the-internet.herokuapp.com/");
         driver.findElement(By.linkText("Infinite Scroll")).click();
